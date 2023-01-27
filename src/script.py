ce8b875823f6224b117bd6fe2dbb23508274c490
Felix Ringe <felix.ringe@fu-berlin.de>
2023-01-27 12:18:12 +0100

    initial commit

---
+++ src/script.py
@@ -0,0 +1,58 @@
+import csv
+import sys
+import termios
+import tty
+
+def getch():
+    """Get a single character from the user without the need to press enter."""
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+# Open the CSV file and read the data
+with open("data/sample_data.csv", "r") as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = [row for row in reader]
+
+# Initialize a variable to store the next unique ID
+next_id = 1
+
+# Iterate through the data
+for row in data:
+    target_string = row["target_string"]
+    matched_string = row["matched_string"]
+
+    # Print the strings and ask the user for input
+    print("Target:", target_string)
+    print("Matched:", matched_string)
+    print("Are these the same company? (n/1/2/q)")
+
+    while True:
+        user_input = getch().lower()
+        if user_input in ["n", "1", "2"]:
+            break
+        elif user_input == "q":
+            sys.exit("Exit the script")
+        else:
+            print("Invalid input. Please enter 'n', '1', '2' or 'q'")
+
+    # Update the company_id based on the user input
+    if user_input == "n":
+        row["company_id"] = "0"
+    elif user_input in ["1", "2"]:
+        row["company_id"] = str(next_id)
+        next_id += 1
+    
+    # Write the updated data to the CSV file
+    with open("data/sample_data.csv", "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=reader.fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+        
+    print("Data written to file.")
+
